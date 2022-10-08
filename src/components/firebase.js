a5515396c2642a272a457b91f3be6e9f13e51c3c
jeandieb <johnnydib15@gmail.com>
2022-10-07 21:22:28 -0700

    add image upload
    selected image is uploaded to firebase STORAGE (not realtime database) and its name is set to be the user uid

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,6 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage"
+
 //import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
@@ -18,5 +20,6 @@ const firebaseConfig = {
   const app = initializeApp(firebaseConfig);
   export const auth = getAuth(app);
   export const db = getDatabase(app);
+  export const storage = getStorage(app);
     //const analytics = getAnalytics(app);
   
